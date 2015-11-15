a45e3edf0cf91137e855f4cb31ed3cd709c79257
Eedrah <eedrah@users.noreply.github.com>
2015-11-15 23:35:02 +1300

    write sudocode

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+label = input
+
+Load the text of the caller's file
+
+Look for "// LABEL ${label}"
+
+keep the lines until // END LABEL ${label}
+
+eval the lines
+
+LABEL blah
+
+goto(blah);
+END LABEL
+
+*/
+
+var goto = function (label) {
+
+};
