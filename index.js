3d338f4d5733d06bd80eecf6aab3026b384a423a
Eedrah <eedrah@users.noreply.github.com>
2015-11-20 09:24:23 +1300

    start piping logic

--- index.js
+++ index.js
@@ -16,6 +16,39 @@ END LABEL
 
 */
 
-var goto = function (label) {
+var callsite = require('callsite');
+var stream = require('readable-stream');
+var fs = require('fs');
+var split = require('split');
 
+var goto = function (label) {
+    var stack = callsite();
+    var callerFile = stack[1].getFileName();
+
+    var inLabel = false;
+    fs.createReadStream(callerFile)
+        .pipe(split())
+        .pipe(new stream.Transform({
+            transform: function (chunk, encoding, next) {
+                var line = chunk.toString();
+                console.log(line);
+                if (line === '// LABEL END ' + label) {
+                    this.push(null);
+                    this.pause();
+                    return;
+                } else if (inLabel) {
+                    console.log(line);
+                    // push
+                } else if(line === '// LABEL ' + label) {
+                    inLabel = true;
+                }
+                console.log('iteration');
+                next();
+            },
+            flush: function (done) {
+                console.log('in flush');
+            }
+        }));
 };
+
+module.exports = goto;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var goto = require('../index');
+
+// LABEL test
+
+console.log('start test');
+goto('test');
+// LABEL END test
+console.log('end test');
